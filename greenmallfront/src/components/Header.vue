25b277853bdd4e9d009bb4d685aaae7826fa0aa0
HG <HongGuang7252@139.com>
2023-03-02 20:10:36 +0800

    HG1.1

---
+++ greenmallfront/src/components/Header.vue
@@ -0,0 +1,157 @@
+<template>
+  <div>
+    <!--第一行布局-->
+    <el-row style="color: white" :gutter="20">
+      <el-col :span="4">
+        <div class="grid-content bg-purple">1</div>
+      </el-col>
+      <el-col :span="4">
+        <div style="color:green" class="grid-content bg-purple-light">
+          <span>欢迎来到绿色助农商城！</span>
+        </div>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple">1</div>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple-light">1</div>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple">
+          <!--登录弹窗-->
+          <div>
+            <!--登录按钮-->
+            <el-button type="text"
+                       @click="dialogVisible1 = true"
+                       style="line-height: 5px;color: #FFCC33">请先登录？
+            </el-button>
+            <!--注册按钮-->
+            <el-button type="text"
+                       @click="dialogVisible2 = true"
+                       style="line-height: 5px;color: #FFCC33">免费注册
+            </el-button>
+            <div>
+              <!--登录提示框-->
+              <el-dialog
+                title="请登录"
+                :visible.sync="dialogVisible1"
+                width="30%"
+                :before-close="handleClose">
+                <span>请输入用户名密码</span>
+                <span slot="footer" class="dialog-footer">
+
+                   <div>
+                   </div>
+
+                   <el-button @click="dialogVisible1 = false">取 消</el-button>
+                   <el-button type="primary" @click="userDelu">登录</el-button>
+                   </span>
+              </el-dialog>
+              <!--注册提示框-->
+              <el-dialog
+                title="注册"
+                :visible.sync="dialogVisible2"
+                width="30%"
+                :before-close="handleClose">
+                <span>注册用户</span>
+                <span slot="footer" class="dialog-footer">
+                    <div>
+
+                    </div>
+                   <el-button @click="dialogVisible2 = false">取 消</el-button>
+                   <el-button type="primary" @click="dialogVisible2 = false">注 册</el-button>
+                   </span>
+              </el-dialog>
+            </div>
+          </div>
+        </div>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple-light">1</div>
+      </el-col>
+    </el-row>
+    <!--      第二行布局-->
+    <el-row style="color: aquamarine;background-color:red;height: 100px" :gutter="20">
+      <el-col :span="4">
+        <div class="grid-content bg-purple">2</div>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple-light">2</div>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple">2</div>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple-light">2</div>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple">2</div>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple-light">2</div>
+      </el-col>
+    </el-row>
+    <div style="display: flex;justify-content:center">
+      <el-menu :default-active="activeIndex"
+               class="el-menu-demo"
+               mode="horizontal"
+               @select="handleSelect">
+        <el-menu-item index="1"><span style="font-size: 20px">首页</span></el-menu-item>
+        <el-menu-item index="2"><span style="font-size: 20px">首页</span></el-menu-item>
+        <el-menu-item index="3"><span style="font-size: 20px">首页</span></el-menu-item>
+        <el-menu-item index="4"><span style="font-size: 20px">首页</span></el-menu-item>
+      </el-menu>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {//暴露当前组件
+  data() {
+    return {
+      activeIndex: '1',
+      activeIndex2: '1',
+      //对话框状态
+      dialogVisible1: false,
+      dialogVisible2: false,
+
+      //  登录表单
+      user: {},
+    };
+
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    },
+
+    //登录注册确认关闭
+    handleClose(done) {
+      this.$confirm('确认关闭？')
+        .then(_ => {
+          done();
+        })
+        .catch(_ => {
+        });
+    },
+
+    userDelu() {
+      console.log(this.user);
+      // 发送axios请求
+      this.$http.get("http://localhost:8080/user", this.user).then(res => {
+        console.log(res);
+        if (res.data.user != null) {
+          //切换路由
+          this.$router.push("/test")
+        }
+      })
+    }
+
+
+  }
+}
+</script>
+
+<style>
+
+</style>
