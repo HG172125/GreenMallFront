c17f97a8b8d2cdc7aec9f3c86029e6020cf408b9
HG <HongGuang7252@139.com>
2023-03-05 11:55:05 +0800

    remove cebain

--- greenmallfront/src/components/NavBar.vue
+++ greenmallfront/src/components/NavBar.vue
@@ -6,11 +6,28 @@
       @select="getKey"
       mode="horizontal">
       <el-menu-item index=""><span style="font-size: 20px">首页</span></el-menu-item>
-      <el-menu-item index="2" @click="toUrl"><span style="font-size: 20px">商城</span></el-menu-item>
+      <el-submenu
+        index="7">
+        <template slot="title">
+          <span style="font-size: 20px">农产品</span>
+        </template>
+
+        <el-submenu index="7-1">
+          <template slot="title">蔬菜</template>
+          <el-menu-item index="7-1-1">白菜</el-menu-item>
+        </el-submenu>
+
+        <el-submenu index="7-2">
+          <template slot="title">水果</template>
+          <el-menu-item index="7-2-1">白菜</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <!--      <el-menu-item index="2" @click="toUrl"><span style="font-size: 20px">商城</span></el-menu-item>-->
       <el-menu-item index="3" @click="toUrl"><span style="font-size: 20px">文章</span></el-menu-item>
       <el-menu-item index="4" @click="toUrl"><span style="font-size: 20px">我的账户</span></el-menu-item>
       <el-menu-item index="5" @click="toUrl"><span style="font-size: 20px">购物车</span></el-menu-item>
       <el-menu-item index="6" @click="toUrl"><span style="font-size: 20px">加入我们</span></el-menu-item>
+
     </el-menu>
   </div>
 </template>
